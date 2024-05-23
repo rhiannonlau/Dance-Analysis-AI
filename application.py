6ac6b245915a396ba5430e9f1a125079ebd62e1f
rhiannonlau <rhiannoncglau@gmail.com>
2024-05-23 14:18:59 -0400

    var and lib initialization

--- application.py
+++ application.py
@@ -1,1 +1,9 @@
+import pandas as panda
+import cv2
+import time
+from datetime import datetime
 
+initialState = None
+motionTrackList = [None, None]
+motionTime = []
+dataFrame = panda.DataFrame(columns = ["Initial", "Final"])
